bd2935579a6fd2b4180d2f3c5dfc682a11b41551
programin.saifulislam@gmail.com <programin.saifulislam@gmail.com>
2024-06-12 20:20:29 +0600

    added to user model file

---
+++ dist/app/Modules/User/User.model.js
@@ -0,0 +1,31 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.UserModel = void 0;
+const mongoose_1 = require("mongoose");
+const UserSchema = new mongoose_1.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    role: {
+        type: String,
+        enum: ["user", "admin"]
+    },
+    passwors: {
+        type: String
+    },
+    phone: {
+        type: String,
+        required: true
+    },
+    address: {
+        type: String,
+        required: true
+    }
+});
+exports.UserModel = (0, mongoose_1.model)("user", UserSchema);
---
+++ src/app/Modules/User/User.model.ts
@@ -0,0 +1,32 @@
+import { Schema, model } from "mongoose";
+import { TUser } from "./User.interface";
+
+const UserSchema = new Schema<TUser>({
+    name:{
+        type:String,
+        required:true
+    },
+    email:{
+        type : String,
+        required:true,
+        unique:true
+    },
+    role:{
+        type:String,
+        enum:["user", "admin"]
+    },
+    passwors:{
+        type:String
+    },
+    phone:{
+        type:String,
+        required:true
+    },
+    address:{
+        type:String,
+        required:true
+    }
+})
+
+
+export const UserModel = model<TUser>("user", UserSchema)
