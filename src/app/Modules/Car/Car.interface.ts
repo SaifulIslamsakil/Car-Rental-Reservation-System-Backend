52296ba0f5a6d68ff034b445824483011bbe5877
programin.saifulislam@gmail.com <programin.saifulislam@gmail.com>
2024-06-12 21:36:04 +0600

    added to car interface file

---
+++ src/app/Modules/Car/Car.interface.ts
@@ -0,0 +1,12 @@
+
+
+export type TCar = {
+    name:string;
+    description:string;
+    color:string;
+    isElectric:boolean;
+    status:"available" | "not available";
+    features:string[];
+    pricePerHour:number;
+    isDeleted :boolean
+}
