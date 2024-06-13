b8f42b4da7b2515ffa3f9532b50cdc2fe3f8a3da
programin.saifulislam@gmail.com <programin.saifulislam@gmail.com>
2024-06-13 22:08:02 +0600

    handel mongoose duplicate error

---
+++ src/app/Errors/HandelMongooseDuplicateError.ts
@@ -0,0 +1,23 @@
+import mongoose from "mongoose";
+import { TErrorSources, TGenericErrorResponse } from "../Interface/Error.interface";
+
+const handelMongooseDuplicateError = (err: any):TGenericErrorResponse =>{
+      // Extract value within double quotes using regex
+      const match = err.message.match(/"([^"]*)"/);
+
+      // The extracted value will be in the first capturing group
+      const extractedMessage = match && match[1];
+      const errorSources: TErrorSources = [
+        {
+            path: '',
+            message: `${extractedMessage} is already exists`,
+        },
+    ];
+
+    const statusCode = 400
+    return {
+        statusCode: statusCode,
+        message: "Invalid ID",
+        errorSources
+    }
+}
